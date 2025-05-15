3ccef3e98d14686c5841e1511db24b60c53313e7
galangrifki <galangdecimo112@gmail.com>
2025-05-15 18:06:39 +0700

    Perbaiki api add dan update untuk menggunakan model request

---
+++ AegislabsProjectAPI/Models/ModelRequest/KRSRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace AegislabsProjectAPI.Models.ModelRequest
+{
+    public class KRSRequest
+    {
+        public Guid MahasiswaId { get; set; }
+        public Guid MataKuliahId { get; set; }
+        public int Semester { get; set; }
+        public string TahunAjaran { get; set; }
+    }
+}
